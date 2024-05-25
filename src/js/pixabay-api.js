799c4686bcbd723ca7bc8aacbd81d9156f339bcd
kostiukevechdima <kostiukevechdima@gmail.com>
2024-05-25 03:41:28 +0300

    commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,21 @@
+const API_KEY = '43705346-f08330685c72fc18a8a8b3aad';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export const fetchPhotos = searchImage => {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: searchImage,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  return fetch(`${BASE_URL}/?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+    return response.json();
+  });
+};
